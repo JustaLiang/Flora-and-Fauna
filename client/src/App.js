7009cbc97117ce99fc23b5b4b3bbfd35ce59d65e
Justa(梁泊伸) <justa@dynacolor.com.tw>
2021-05-21 17:23:30 +0800

    Add front-end: changeDirection()

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,18 @@ import {getEthereum} from "./getEthereum"
 class App extends React.Component {
 
     state = {
+        //--- base
         web3: null,
         accounts: null,
         chainid: null,
         laboratory: null,
+        //--- display
         yourPlants: {},
-        plantID: 0,
+        plantInfo: [],
+        //--- input
+        aggregatorIndex: 0,
+        queryPlantID: 0,
+        changePlantID: 0,
     }
 
     componentDidMount = async () => {
@@ -46,32 +52,32 @@ class App extends React.Component {
 
     loadInitialContracts = async () => {
         
-        const {chainid} = this.state
+        const { chainid } = this.state
 
         if (chainid <= 42) {
             // Wrong Network!
             return
         }
 
-        const laboratory = await this.loadContract("dev", "Laboratory")
+        const laboratory = await this.loadContract("dev", "Laboratory", 0)
 
         if (!laboratory) {
             return
         }
 
-        this.setState({laboratory})
+        this.setState({ laboratory })
 
         await this.getPlantList()
     }
 
-    loadContract = async (chain, contractName) => {
+    loadContract = async (chain, contractName, index) => {
         // Load a deployed contract instance into a web3 contract object
-        const {web3} = this.state
+        const { web3 } = this.state
 
         // Get the address of the most recent deployment from the deployment map
         let address
         try {
-            address = map[chain][contractName][0]
+            address = map[chain][contractName][index]
         } catch (e) {
             console.log(`Couldn't find any deployed contract "${contractName}" on the chain "${chain}".`)
             return undefined
@@ -90,20 +96,75 @@ class App extends React.Component {
     }
 
     getPlantList = async () => {
-        const {accounts, laboratory} = this.state
+        const { accounts, laboratory } = this.state
+        if (accounts.length == 0) {
+            return
+        }
         const plantIDs = await laboratory.methods.getPlantsByOwner(accounts[0]).call()
-        let updatePlants = {}
+        let yourPlants = {}
         for (let id of plantIDs)
         {
-            updatePlants[id] = await laboratory.methods.showPlant(id).call()
+            yourPlants[id] = await laboratory.methods.showPlant(id).call()
         }
-        this.setState({yourPlants: updatePlants})
+        this.setState({ yourPlants })
+    }
+
+    plantSeed = async (e) => {
+        const { accounts, laboratory, aggregatorIndex } = this.state
+        e.preventDefault()
+        const aggs = map["dev"]["MockV3Aggregator"]
+        if (aggregatorIndex >= aggs.length || aggregatorIndex < 0) {
+            console.log(`invalid aggregator (max: ${aggs.length - 1})`)
+            return
+        }
+        const aggregator = aggs[aggregatorIndex]
+        await laboratory.methods.seed(aggregator, true).send({ from: accounts[0] })
+            .on("receipt", async () => {
+                await this.getPlantList()
+            })
+            .on("error", async () => {
+                console.log("error")
+            })
+    }
+
+    showCertainPlant = async (e) => {
+        const { laboratory, queryPlantID } = this.state
+        e.preventDefault()
+        const pid = parseInt(queryPlantID)
+        if (isNaN(pid)) {
+            alert("invalid plant ID")
+            return
+        }
+        try {
+            this.setState({ plantInfo: await laboratory.methods.showPlant(pid).call() })
+        } catch (e) {
+            console.log(e)
+            this.setState({ plantInfo: ["not exists"] })
+        }
+    }
+
+    changeDirection = async (e) => {
+        const { accounts, laboratory, changePlantID } = this.state
+        e.preventDefault()
+        const pid = parseInt(changePlantID)
+        if (isNaN(pid)) {
+            alert("invalid plant ID")
+            return
+        }
+        await laboratory.methods.changePhototropism(pid).send({ from: accounts[0] })
+            .on("receipt", async () => {
+                await this.getPlantList()
+            })
+            .on("error", async () => {
+                console.log("error")
+            })
     }
 
     render() {
         const {
-            web3, accounts, chainid,
-            laboratory, yourPlants, plantID
+            web3, accounts, chainid, laboratory,
+            yourPlants, plantInfo,
+            aggregatorIndex, queryPlantID, changePlantID
         } = this.state
 
         if (!web3) {
@@ -120,34 +181,60 @@ class App extends React.Component {
 
         const isAccountsUnlocked = accounts ? accounts.length > 0 : false
         const plantList = Object.keys(yourPlants).map(function (id) {
-            return <li> {id}: {yourPlants[id].join(" | ")}</li>
+            return <li key={id}> {id}: {yourPlants[id].join(" | ")}</li>
         });
 
         return (<div className="App">
             {
                 !isAccountsUnlocked ?
-                    <p><strong>Connect with Metamask and refresh the page to
-                        be able to edit the storage fields.</strong>
+                    <p><strong>Connect with Metamask and refresh the page.</strong>
                     </p>
                     : null
             }
 
-            <h2>Crypto Gardener</h2>
-            <div>Your plants: <br/> {plantList}</div>
+            <h1>Crypto Gardener</h1>
+            <div> <h3>your plants</h3>{plantList}</div>
             <br/>
-            <form onSubmit={() => this.getPlantList()}>
+            <form onSubmit={(e) => this.plantSeed(e)}>
                 <div>
-                    <label>Update: </label>
-                    <br/>
+                    <h3>plant a seed</h3>
                     <input
-                        name="plantID"
+                        name="aggregatorIndex"
                         type="text"
-                        value={plantID}
-                        onChange={(e) => this.setState({plantID: e.target.value})}
+                        value={aggregatorIndex}
+                        onChange={(e) => this.setState({ aggregatorIndex: e.target.value })}
+                    />
+                    <br />
+                    <button type="submit" disabled={!isAccountsUnlocked}>seed</button>
+                </div>
+            </form>
+            <br/>
+            <form onSubmit={(e) => this.showCertainPlant(e)}>
+                <div>
+                    <h3>query a plant</h3>
+                    <input
+                        name="queryPlantID"
+                        type="text"
+                        value={queryPlantID}
+                        onChange={(e) => this.setState({queryPlantID: e.target.value})}
                     />
                     <br/>
-                    <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
-
+                    <button type="submit" disabled={!isAccountsUnlocked}>query</button>
+                </div>
+            <div> <br/>{plantInfo.join(" | ")}</div>
+            </form>
+            <br/>
+            <form onSubmit={(e) => this.changeDirection(e)}>
+                <div>
+                    <h3>change a plant</h3>
+                    <input
+                        name="changePlantID"
+                        type="text"
+                        value={changePlantID}
+                        onChange={(e) => this.setState({ changePlantID: e.target.value })}
+                    />
+                    <br />
+                    <button type="submit" disabled={!isAccountsUnlocked}>change</button>
                 </div>
             </form>
         </div>)
