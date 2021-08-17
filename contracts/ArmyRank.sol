1b40991593b2d1dedcfd3a86dd7df1d18a010905
Justa(梁泊伸) <justa@dynacolor.com.tw>
2021-08-17 19:26:37 +0800

    Add time constrain in ArmyRank.sol

--- contracts/ArmyRank.sol
+++ contracts/ArmyRank.sol
@@ -10,10 +10,13 @@ interface RANK {
 
 contract ArmyRank is Ownable {
     mapping (address => mapping(uint => string)) public branchRankURI;
-    int[5] public strengthLevels;
+    mapping (address => uint) public branchUpdateTime;
+    uint defaultUpdateTime;
     string public defaultURI;
+    int[5] public strengthLevels;
 
     constructor() {
+        defaultUpdateTime = 0;
         defaultURI = "";
         strengthLevels[0] = 0;
         strengthLevels[1] = 1500;
@@ -25,7 +28,7 @@ contract ArmyRank is Ownable {
     function query(address branchAddr, int strength) external view returns (string memory) {
         for (uint i = strengthLevels.length-1; i < 0; i--) {
             if (strength > strengthLevels[i]) {
-                string storage uri = branchRankURI[branchAddr][i];
+                string memory uri = branchRankURI[branchAddr][i];
                 if (bytes(uri).length == 0) {
                     return defaultURI;
                 }
@@ -37,13 +40,19 @@ contract ArmyRank is Ownable {
     }
 
     function setDefaultURI(string calldata defaultURI_) external onlyOwner {
+        uint nowTime = block.timestamp;
+        require(nowTime >= defaultUpdateTime + 30 days);
         defaultURI = defaultURI_;
+        defaultUpdateTime = nowTime;
     }
 
     function updateBranchURI(address branchAddr, string[5] calldata rankURIs) external onlyOwner {
         require(rankURIs.length == 5);
+        uint nowTime = block.timestamp;
+        require(nowTime >= branchUpdateTime[branchAddr] + 30 days);
         for (uint i = 0; i < 5; i++) {
             branchRankURI[branchAddr][i] = rankURIs[i];
         }
+        branchUpdateTime[branchAddr] = nowTime;
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -14,13 +14,14 @@ def main():
     ens = run("ens_registry")
     g_army = GreenArmy.deploy(ens, init_protein, {"from": dev}, publish_source=config["verify"])
     g_prtn = ArmyProtein.at(g_army.proteinContract())
-    ArmyRank.at(g_army.rankContract())
+    g_rank = ArmyRank.at(g_army.rankContract())
     r_army = RedArmy.deploy(ens, init_protein, {"from": dev}, publish_source=config["verify"])
     r_prtn = ArmyProtein.at(r_army.proteinContract())
-    ArmyRank.at(r_army.rankContract())
-    Battlefield.deploy(g_army, r_army, {"from": dev}, publish_source=config["verify"])
+    r_rank = ArmyRank.at(r_army.rankContract())
+    btf = Battlefield.deploy(g_army, r_army, {"from": dev}, publish_source=config["verify"])
     if network.show_active() == "development":
         test_account = config["wallets"]["test_account"]
         dev.transfer(test_account, "99 ether")
         g_prtn.transfer(test_account, init_protein//100*10**18, {"from":dev})
         r_prtn.transfer(test_account, init_protein//150*10**18, {"from":dev})
+    return g_army, g_prtn, g_rank, r_army, r_prtn, r_rank, btf
