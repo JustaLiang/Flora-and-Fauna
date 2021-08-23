ce9bec79c1ec394087bdec99789c7e1a05044d18
JustaJunk <tcfsh.crazer@gmail.com>
2021-08-23 17:45:18 +0800

    Modify filename

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,7 +5,8 @@ from brownie import (
 
 init_enhancer = 7777777
 power_levels = [0, 1010, 1020, 1030, 1040]
-json_names = ['level_1.json','level_2.json','level_3.json','level_4.json','level_5.json']
+flora_images = ['flora_1.jpg','flora_2.jpg','flora_3.jpg','flora_4.jpg','flora_5.jpg']
+fauna_images = ['fauna_1.jpg','fauna_2.jpg','fauna_3.jpg','fauna_4.jpg','fauna_5.jpg']
 
 def main():
     if network.show_active() != "development":
@@ -14,10 +15,10 @@ def main():
             return
     dev = run("dev_account")
     ens = run("ens_registry")
-    g_army = FloraArmy.deploy(ens, init_enhancer, power_levels, json_names, {"from": dev}, publish_source=config["verify"])
+    g_army = FloraArmy.deploy(ens, init_enhancer, power_levels, flora_images, {"from": dev}, publish_source=config["verify"])
     g_enhr = ArmyEnhancer.at(g_army.enhancerContract())
     g_rank = ArmyRank.at(g_army.rankContract())
-    r_army = FaunaArmy.deploy(ens, init_enhancer, power_levels, json_names, {"from": dev}, publish_source=config["verify"])
+    r_army = FaunaArmy.deploy(ens, init_enhancer, power_levels, fauna_images, {"from": dev}, publish_source=config["verify"])
     r_enhr = ArmyEnhancer.at(r_army.enhancerContract())
     r_rank = ArmyRank.at(r_army.rankContract())
     btf = Battlefield.deploy(g_army, r_army, {"from": dev}, publish_source=config["verify"])
