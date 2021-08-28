73144c4f7707fdc7dd9bf54fd1c537024eb0fa67
Jeffrey Lin <qwertzxc1478963@gmail.com>
2021-08-28 23:30:41 +0800

    Add component: Proposal.js ProposalList.js

--- client/src/components/Field.js
+++ client/src/components/Field.js
@@ -1,12 +1,16 @@
 import React,{useState} from 'react'
-import {CardHeader, Card, CardContent, CardActions, Box, Avatar, Typography, IconButton, Button, Snackbar } from '@material-ui/core';
+import {CardHeader, Card, CardContent, CardActions, Box, Avatar, Typography, IconButton, Button, Snackbar,Dialog, DialogTitle, DialogActions } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
 export default function Field() {
     const [shadow,setShadow] = useState(1)
     const [open,setOpen] = useState(false)
     const handleClick = () => {
         setOpen(true)
-    }
+    };
+    const handleClose = () => {
+        setOpen(false);
+      };
     return (
         <div  >
             <Card 
@@ -18,6 +22,18 @@ export default function Field() {
            alignItems:'center',justifyContent:'center'}}>
               <EmojiFlagsIcon style={{width:75,height:75}}/> 
             </Card>
+            <Dialog open={open} style={{ textAlign: 'center' }} onClose={handleClose}>
+              <DialogTitle id="form-dialog-title" style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+                  <Box><Typography>Field Info</Typography></Box>
+                  <Box>
+              <IconButton onClick={handleClose} ><CloseIcon/>
+              </IconButton>
+              </Box>
+              </DialogTitle>
+              <Box style={{height:500,width:500}}> 
+
+              </Box>
+            </Dialog>
             
         </div>
     )
