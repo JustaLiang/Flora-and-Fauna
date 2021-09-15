1e7d6366ff28ecc94f1d2d097e7db6c08317ecd5
Jeffrey Lin <qwertzxc1478963@gmail.com>
2021-09-16 00:40:47 +0800

    add address copy

--- frontend/src/components/PersonalInfo.tsx
+++ frontend/src/components/PersonalInfo.tsx
@@ -1,10 +1,13 @@
-import { Box, Container, Paper, Typography } from "@material-ui/core";
+import { Box, Container, IconButton, Paper, Snackbar, SnackbarCloseReason, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React, { useContext, useEffect, useState } from "react";
-import { FloraArmyContext, FaunaArmyContext, CurrentAddressContext, ArmyEnhancerContext } from "../hardhat/SymfoniContext";
-import { BalanceContext } from "../pages/Factory";
+import FilterNoneIcon from '@material-ui/icons/FilterNone';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import { BigNumber } from "ethers";
+import React, { useContext, useEffect, useState } from "react";
+import { ArmyEnhancerContext, CurrentAddressContext, FaunaArmyContext, FloraArmyContext } from "../hardhat/SymfoniContext";
 import { ArmyEnhancer } from "../hardhat/typechain/ArmyEnhancer";
+import { BalanceContext } from "../pages/Factory";
+
 
 const useStyle = makeStyles((theme) => ({
   paper: {
@@ -13,6 +16,9 @@ const useStyle = makeStyles((theme) => ({
   },
 }));
 
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 interface Props {
   isFauna: boolean
 }
@@ -25,6 +31,7 @@ export default function PersonalInfo(properties: Props) {
   const [enhancer, setEnhancer] = useState<ArmyEnhancer>();
   const account = useContext(CurrentAddressContext);
   const [balance, setBalance] = useContext(BalanceContext);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const fetchEnhancerBalance = async () => {
@@ -36,7 +43,7 @@ export default function PersonalInfo(properties: Props) {
         const decimals = await enhancer.decimals();
         setEnhancer(enhancer);
         setBalance(rawBalance.div(BigNumber.from(10).pow(decimals)));
-      } 
+      }
       if (!isFauna && floraArmy.instance) {
         const enhrAddr = await floraArmy.instance.enhancerContract();
         const enhancer = armyEnhancer.factory.attach(enhrAddr);
@@ -49,6 +56,23 @@ export default function PersonalInfo(properties: Props) {
     fetchEnhancerBalance();
   }, [isFauna, account, armyEnhancer, faunaArmy, floraArmy, setBalance])
 
+  const handleBarClose = (event: React.SyntheticEvent<any>, reason: SnackbarCloseReason) => {
+    event?.preventDefault();
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleAlertClose = (event: React.SyntheticEvent<Element, Event>) => {
+    event.preventDefault();
+    setOpen(false);
+  }
+  const onCopy = () => {
+    if (!enhancer) return;
+    navigator.clipboard.writeText(enhancer.address);
+    setOpen(true);
+  }
   return (
     <Container>
       <Box display="flex" flexDirection="row" maxWidth={1000} minWidth={800}>
@@ -65,8 +89,13 @@ export default function PersonalInfo(properties: Props) {
       >
         {account[0] ? (
           <>
-            <Typography>{isFauna?"Hemoglobin (HGB)":"Chlorophyll (CHL)"} contract: {enhancer?.address}</Typography>
-            <Typography>Your {isFauna?"Hemoglobin":"Chlorophyll"} Balance: {balance.toString()}</Typography>
+            <Typography>{isFauna ? "Hemoglobin (HGB)" : "Chlorophyll (CHL)"} contract: {enhancer?.address} <IconButton onClick={onCopy}><FilterNoneIcon /></IconButton> </Typography>
+            <Typography>Your {isFauna ? "Hemoglobin" : "Chlorophyll"} Balance: {balance.toString()}</Typography>
+            <Snackbar open={open} autoHideDuration={1000} onClose={handleBarClose}>
+              <Alert onClose={handleAlertClose} severity="success">
+                Contract address copied!
+              </Alert>
+            </Snackbar>
           </>
         ) : (
           <>
